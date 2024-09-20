5f39d97d8695482c9134b4134369cee677efaf5f
Harsh <harshradadiya9999@gmail.com>
2024-09-20 17:33:56 +0530

    update

--- tokenswaping/src/app/page.tsx
+++ tokenswaping/src/app/page.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
+import Page from "./swap/page";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">App</div>
-    </main>
-  );
+  return <Page />;
 }
